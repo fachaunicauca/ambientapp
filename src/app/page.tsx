2c00d2329447695a373a1884e4680b365cbe7f0a
cmperdomo17 <carlosmarioperdomo555@gmail.com>
2025-02-05 21:46:06 -0500

    Primera versión Interfaz Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,45 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="flex gap-5 w-full h-screen p-5">
-      <div className="flex flex-col gap-3 items-center justify-center w-1/2 border-r-2 border-blueDark">
-        <h1 className="text-2xl font-bold text-blueDark">
-          Pasantia Ambiental
-        </h1>
-        <Button
-          className="px-5 py-2 rounded-3xl"
-        >
-          Login
-        </Button>
+    <div className="flex gap-2 w-full h-screen p-2">
+      <div className="flex flex-col gap-3 items-center justify-center w-1/2">
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl text-blueDark font-bold text-center">
+            Laboratorio de Ingeniería Ambiental y Sanitaria
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3 w-1/2 py-36">
+          <Input placeholder="Usuario" />
+          <Input type="password" placeholder="Contraseña" />
+          <div className="flex items-center space-x-2 pb-2">
+            <Checkbox id="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Recordar por 30 días
+            </label>
+          </div>
+          <Button className="px-5 py-2 rounded-3xl mt-2">Iniciar Sesión</Button>
+          <p className="text-gray-700 text-center">
+            ó
+          </p>
+          <Button className="flex gap-2 px-5 py-2 rounded-3xl bg-red-500 text-blueDark hover:text-white group transition-all duration-500">
+            <Image src="/google.svg" alt="Google" height={20} width={20} className="block group-hover:hidden" />
+            <Image src="/googlewhite.svg" alt="Google" height={20} width={20} className="hidden group-hover:block" />
+            <h1>Iniciar Sesión con Google</h1>
+          </Button>
+
+        </div>
       </div>
-      <div className="bg-blueDark rounded-3xl w-1/2 flex items-center justify-center">
-        <Image
-          src={"/ambiental.png"}
-          alt="Pasantia Ambiental"
-          height={500}
-          width={500}
-          className="rounded-3xl"
-        />
+      <div className="bg-[url('/bg.svg')] bg-cover rounded-xl flex flex-col gap-2 w-4/5">
+        <div className="flex flex-col gap-5 p-6 items-center justify-center">
+          <Image src={"/u2.png"} alt="Logo" height={200} width={200} />
+        </div>
       </div>
     </div>
   );
