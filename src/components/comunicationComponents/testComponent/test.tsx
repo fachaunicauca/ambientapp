4a62628f4c1efefc9eef9df480335f765a6658b6
Jdcarsa <jdcardenasvld@hotmail.com>
2025-05-08 11:42:31 -0500

    fix: correciones

--- src/components/comunicationComponents/testComponent/test.tsx
+++ src/components/comunicationComponents/testComponent/test.tsx
@@ -99,8 +99,6 @@ export default function Evaluation({ studentCode, subjectName, teacherName, ques
     const endIndex = startIndex + questionsPerPage;
     const paginatedQuestions = questions.slice(startIndex, endIndex);
 
-    const allAnswered = Object.keys(answers).length === questions.length;
-
     return (
         <div className="p-6">
             <h3 className="text-center text-lg font-bold tracking-1.25 leading-19 m-2">
